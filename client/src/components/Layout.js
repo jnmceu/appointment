3bc78c35da33d30ba21c18ed4a5a8989eaca54b9
jnmceu <jaoschnathanmendoza@gmail.com>
2024-05-14 21:37:13 +0800

    Layout Changes for the SIdebar

--- client/src/components/Layout.js
+++ client/src/components/Layout.js
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import "../layout.css";
-import {Link, useLocation} from 'react-router-dom';
+import { Link, useLocation } from "react-router-dom";
 
 function Layout({ children }) {
+  const [collapsed, setCollapsed] = useState(false);
   const location = useLocation();
   const userMenu = () => [
     {
@@ -37,24 +38,42 @@ function Layout({ children }) {
   return (
     <div className="main">
       <div className="d-flex layout">
-        <div className="sidebar">
+        <div className='sidebar'>
           <div className="sidebar-header">
-            <h1>CEU</h1><p>Online Appointment</p>
+            <h1>CEU</h1>
 
             <div className="menu">
               {menuToBeRendered.map((menu) => {
-                const isActive = location.pathname === menu.path
-                return <div className={`d-flex menu-item ${isActive && 'active-menu-item'}`}>
+                const isActive = location.pathname === menu.path;
+                return (
+                  <div
+                    className={`d-flex menu-item ${
+                      isActive && "active-menu-item"
+                    }`}
+                  >
                     <i className={menu.icon}></i>
-                    <Link to={menu.path}>{menu.name}</Link>
+                    {!collapsed && <Link to={menu.path}>{menu.name}</Link>}
                   </div>
+                );
               })}
             </div>
           </div>
         </div>
 
         <div className="content">
-          <div className="header">header</div>
+          <div className="header">
+            {collapsed ? (
+              <i
+                className="ri-menu-2-fill header-action-icon"
+                onClick={() => setCollapsed(false)}
+              ></i>
+            ) : (
+              <i
+                className="ri-close-fill header-action-icon"
+                onClick={() => setCollapsed(true)}
+              ></i>
+            )}
+          </div>
 
           <div className="body">{children}</div>
         </div>
