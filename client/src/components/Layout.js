fdafad4add8d3b8ea4372eb1db80b7ff2b129924
jnmceu <jaoschnathanmendoza@gmail.com>
2024-05-15 18:12:34 +0800

    Further Layout changes

--- client/src/components/Layout.js
+++ client/src/components/Layout.js
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
 import "../layout.css";
 import { Link, useLocation } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 function Layout({ children }) {
   const [collapsed, setCollapsed] = useState(false);
+  const {user} = useSelector((state) => state.user);
   const location = useLocation();
   const userMenu = () => [
     {
@@ -73,8 +75,11 @@ function Layout({ children }) {
                 onClick={() => setCollapsed(true)}
               ></i>
             )}
+            <div className="d-flex align-items-center px-4">
+              <i className="ri-notification-line header-action-icon px-3"></i>
+              <Link className="anchor" to='/profile' style={{ color: 'black' }}>{user?.name}</Link>
+            </div>
           </div>
-
           <div className="body">{children}</div>
         </div>
       </div>
